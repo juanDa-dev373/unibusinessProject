068d226a7d6822813748311eb77df82f9e3d52d3
juanDa-dev373 <teamcracks38@gmail.com>
2024-06-12 20:50:04 -0500

    hola mundo

---
+++ src/main/java/co/unibusiness/unibusiness/UnibusinessApplication.java
@@ -0,0 +1,13 @@
+package co.unibusiness.unibusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnibusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnibusinessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/unibusiness/unibusiness/UnibusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package co.unibusiness.unibusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnibusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
